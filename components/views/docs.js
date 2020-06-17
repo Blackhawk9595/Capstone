c332bb074a2f85d192f1f339ecdddce0714dbcbe
bradleyharper <bradharper123@hotmail.com>
2020-06-16 20:44:42 -0500

    still broken, stopped at addNavListeners

--- components/views/docs.js
+++ components/views/docs.js
@@ -4,8 +4,5 @@ export default () => `
     <h3>Human v Machines</h3>
     <h4>Episode one</h4>
     <h5>The Global Surveillance System</h5>
-
-
     <iframe width="560" height="315" src="https://www.youtube.com/embed/JT-XgUMgqIM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
 `;
--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 import { header, nav, main,footer} from "./components";
 import * as state from "./store";
-import { docs } from "./components/views";
 import  Navigo  from 'navigo';
+import { capitalize } from "lodash";
 
 const router = new Navigo(window.location.origin);
 
-const render = (st=state.home) => {
+router
+  .on({
+    ":page": params => render(state[capitalize(params.page)]),
+    "/": () => render(state.home)
+  })
+  .resolve();
+
+function  render(st=state.home) {
+  console.log(st);
   document.querySelector("#root").innerHTML = `
   ${header()}
   ${nav()}
   ${main(st)}
   ${footer()}
   `;
-  
   router.updatePageLinks(); 
-  addNavListeners();
+  addNavListeners(st);
   };
-  
-router
-  .on({
-    ":page": params => render(state[capitalize(params.page)]),
-    "/": () => render(state.Home)
-  })
-  .resolve();
 
-render();
-
-function addNavListeners() { document.querySelectorAll("nav a").forEach(el => {
+function addNavListeners() { 
+  document.querySelectorAll("nav > ul").forEach(el => {
+    console.log('el', el);
     el.addEventListener("click", event => {
-        event.preventDefault();
-        let page = event.target.textContent;
-        render(state[page]);
+      console.log("clicked");
+      event.preventDefault();
+      let page = event.target.textContent;
+      render(state[page]);
     });
 });
 }
